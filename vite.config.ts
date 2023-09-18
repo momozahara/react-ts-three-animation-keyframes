23d348f01c47ea90fdd20a173c0ee5cab09ab9a0
Pream Pinbut <momozahara@hotmail.com>
2023-09-19 01:30:51 +0700

    remove unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { dependencies } from "./package.json";
-
-function renderChunks(deps: Record<string, string>) {
-  const chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (["three"].includes(key)) {
-      return;
-    }
-    chunks[key] = [key];
-  });
-  return chunks;
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,7 +11,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           three: ["three"],
-          ...renderChunks(dependencies),
         },
       },
     },
